9ec018a9e967f62a5fb3458339d73d577c442d88
jay <jay.160410116139@gmail.com>
2020-07-13 17:58:29 +0530

    React programs

---
+++ webseries/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+ <>
+    <App imgsrc = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.sduV_GJOFkZ_Z2NP2Kux4AHaEK%26pid%3DApi&f=1"
+    title = "Money Heist"
+    category = "Netflix Original Series"
+    link = "https://www.netflix.com/in/title/80192098" />
+
+    <App imgsrc = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.PyjcYawUfd0cxD06pKe2CwHaEK%26pid%3DApi&f=1"
+    title = "Stranger Things"
+    category = "Netflix Original Series"
+    link = "https://www.netflix.com/in/title/80057281"/>
+  
+
+    <App imgsrc = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OAHcNLDZecUc9vdkT5qgxwHaEo%26pid%3DApi&f=1"
+    title = "Dark"
+    category = "Netflix Original Series"
+    link = "https://www.netflix.com/in/title/80100172" />
+  
+    <App imgsrc = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.explicit.bing.net%2Fth%3Fid%3DOIP.mib_N7xedk3vIhxqe5M51wHaEK%26pid%3DApi&f=1"
+    title = "Narcos"
+    category = "Netflix Original Series"
+    link = "https://www.netflix.com/in/title/80025172" />
+    
+    <App imgsrc = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.N6ulMFko8BoozbH3kWCuNAHaEK%26pid%3DApi&f=1"
+    title = "DareDavil"
+    category = "Netflix Original Series"
+    link = "https://www.netflix.com/in/title/80018294" />
+    </>
+    
+    
+    
+    
+    
+    ,
+  
+  
+  document.getElementById('root')
+);
+
